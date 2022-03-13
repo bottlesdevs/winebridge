23220900ce861332d77530c92b7e6de75928cb3d
Pietro di Caprio <pietro@fabricators.ltd>
2022-03-13 16:52:54 +0100

    Print version in Help command

--- src/WineBridge/Commands/Help.cs
+++ src/WineBridge/Commands/Help.cs
@@ -1,9 +1,15 @@
-﻿namespace WineBridge.Commands;
+﻿using System.Reflection;
+
+namespace WineBridge.Commands;
 
 internal class Help : ICommand
 {
     public void Execute(string[]? args)
     {
+        var version = typeof(Program).Assembly
+            .GetCustomAttribute<AssemblyFileVersionAttribute>()
+            ?.Version;
+        Console.WriteLine($"Version: {version}");
         Console.WriteLine("List of commands:");
         foreach (var command in Data.Commands)
         {
